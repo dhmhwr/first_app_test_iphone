e61027c745969b20b62190c8678fc0e0c305422a
wr <1771821366@qq.com>
2026-04-25 22:36:32 +0800

    test_get_path

--- lib/main.dart
+++ lib/main.dart
@@ -1,35 +1,55 @@
 import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
 
-// 程序入口
 void main() {
   runApp(const MyApp());
 }
 
-// 根组件
+// 1. 根组件：固定不变
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // 去掉右上角的debug标签
       debugShowCheckedModeBanner: false,
-      // 应用首页
-      home: Scaffold(
-        // 顶部导航栏
-        appBar: AppBar(title: const Text("我的第一个Flutter程序")),
-        // 页面主体
-        body: const Center(
-          child: Text(
-            "我的小说漫画阅读器\n即将运行在iPhone上",
-            style: TextStyle(
-              fontSize: 24,
-              height: 1.5, // 行高
-              fontWeight: FontWeight.bold,
-            ),
-            textAlign: TextAlign.center,
-          ),
-        ),
+      home: HomePage(),
+    );
+  }
+}
+
+// 2. 主页：动态页面（可以刷新Text）
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+// 3. 真正干活的「状态类」
+class _HomePageState extends State<HomePage> {
+  // 变量：专门存路径文本
+  String showText = "加载中...";
+
+  // 页面初始化：只执行一次
+  @override
+  void initState() {
+    super.initState();
+    // 异步获取路径
+    getApplicationDocumentsDirectory().then((dir) {
+      // 刷新界面，把路径塞进 Text
+      setState(() {
+        showText = dir.path;
+      });
+    });
+  }
+
+  // 页面UI
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("存储路径")),
+      body: Center(
+        // 就这一个 Text 打印路径
+        child: Text(showText),
       ),
     );
   }
